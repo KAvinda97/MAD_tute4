ad15fcf4715fac1690c109e9cede91f421d744a4
KAvinda Geeth <kavindageeth00@gmail.com>
2020-04-01 14:19:49 +0530

    last commit

--- app/src/main/java/com/example/tute4/DBHelper.java
+++ app/src/main/java/com/example/tute4/DBHelper.java
@@ -12,11 +12,13 @@ import java.util.List;
 public class DBHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "UserInfo.db";
 
-    public DBHelper(Context context){super(context,DATABASE_NAME,null,1);}
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String SQL_CREATE_ENTRIES = "CREATE TABLE " +UsersMaster.Users.TABLE_NAME+"("+UsersMaster.Users._ID + "INTEGER PRIMARY KEY," + UsersMaster.Users.COLUMN_NAME_USERNAME+ "TEXT," + UsersMaster.Users.COLUMN_NAME_PASSWORD + "TEXT)";
+        String SQL_CREATE_ENTRIES = "CREATE TABLE " + UsersMaster.Users.TABLE_NAME + "(" + UsersMaster.Users._ID + "INTEGER PRIMARY KEY," + UsersMaster.Users.COLUMN_NAME_USERNAME + "TEXT," + UsersMaster.Users.COLUMN_NAME_PASSWORD + "TEXT)";
         db.execSQL(SQL_CREATE_ENTRIES);
 
     }
@@ -25,27 +27,28 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
-    public void addInfo(String userName, String password){
+
+    public void addInfo(String userName, String password) {
         SQLiteDatabase db = getWritableDatabase();
 
         ContentValues values = new ContentValues();
-        values.put(UsersMaster.Users.COLUMN_NAME_USERNAME,userName);
-        values.put(UsersMaster.Users.COLUMN_NAME_PASSWORD,password);
+        values.put(UsersMaster.Users.COLUMN_NAME_USERNAME, userName);
+        values.put(UsersMaster.Users.COLUMN_NAME_PASSWORD, password);
 
-        long newRowId = db.insert(UsersMaster.Users.TABLE_NAME,null,values);
+        long newRowId = db.insert(UsersMaster.Users.TABLE_NAME, null, values);
     }
 
-    public void readAllInfo(){
+    public void readAllInfo() {
         SQLiteDatabase db = getReadableDatabase();
         String[] projection = {UsersMaster.Users._ID, UsersMaster.Users.COLUMN_NAME_USERNAME, UsersMaster.Users.COLUMN_NAME_PASSWORD};
         String sortOrder = UsersMaster.Users.COLUMN_NAME_USERNAME + "DESC";
 
-        Cursor  cursor = db.query(UsersMaster.Users.TABLE_NAME,projection,null,null,null,null,sortOrder);
+        Cursor cursor = db.query(UsersMaster.Users.TABLE_NAME, projection, null, null, null, null, sortOrder);
 
         List userNames = new ArrayList<>();
         List passwords = new ArrayList<>();
 
-        while (cursor.moveToNext()){
+        while (cursor.moveToNext()) {
             String username = cursor.getString(cursor.getColumnIndexOrThrow(UsersMaster.Users.COLUMN_NAME_USERNAME));
             String password = cursor.getString(cursor.getColumnIndexOrThrow(UsersMaster.Users.COLUMN_NAME_PASSWORD));
             userNames.add(username);
@@ -53,3 +56,22 @@ public class DBHelper extends SQLiteOpenHelper {
         }
 
     }
+
+    public void deleteInfo (String userName){
+        SQLiteDatabase db = getReadableDatabase();
+        String seletion = UsersMaster.Users.COLUMN_NAME_USERNAME + "LIKE ?";
+        String[] seletionArgs = {userName};
+        db.delete(UsersMaster.Users.TABLE_NAME,seletion,seletionArgs);
+    }
+
+    public void updateInfo(String userName, String password) {
+        SQLiteDatabase db = getReadableDatabase();
+        ContentValues values  = new ContentValues();
+        values.put(UsersMaster.Users.COLUMN_NAME_PASSWORD,password);
+
+        String seletion = UsersMaster.Users.COLUMN_NAME_USERNAME + " LIKE ?";
+        String[] seletionArgs = {userName};
+
+        int count = db.update(UsersMaster.Users.TABLE_NAME,values,seletion,seletionArgs);
+    }
+}
